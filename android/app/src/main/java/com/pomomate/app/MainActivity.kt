d4abd2b8b7b94ad0fd15a928e9cbcc0e5d05cd9f
Yusuf <yusuf1999-00@outlook.com>
2026-08-31 23:26:25 +0300

    fix(android): add native MediaProjection service, PiP module, and manifest updates for screen sharing

---
+++ android/app/src/main/java/com/pomomate/app/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.pomomate.app
+import expo.modules.splashscreen.SplashScreenManager
+
+import android.app.PictureInPictureParams
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Bundle
+import android.util.Rational
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+import com.oney.WebRTCModule.WebRTCModuleOptions
+
+import expo.modules.ReactActivityDelegateWrapper
+
+class MainActivity : ReactActivity() {
+
+  companion object {
+    var instance: MainActivity? = null
+    var isInPiPMode: Boolean = false
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Set the theme to AppTheme BEFORE onCreate to support
+    // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    SplashScreenManager.registerOnActivity(this)
+    // @generated end expo-splashscreen
+
+    // Enable WebRTC MediaProjection foreground service for screen sharing
+    val options = WebRTCModuleOptions.getInstance()
+    options.enableMediaProjectionService = true
+
+    super.onCreate(null)
+    instance = this
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "main"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return ReactActivityDelegateWrapper(
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
+  }
+
+  /**
+   * Enter Picture-in-Picture mode with a 16:9 aspect ratio
+   */
+  fun enterPiPMode() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      val params = PictureInPictureParams.Builder()
+        .setAspectRatio(Rational(16, 9))
+        .build()
+      enterPictureInPictureMode(params)
+    }
+  }
+
+  override fun onPictureInPictureModeChanged(
+    isInPictureInPictureMode: Boolean,
+    newConfig: Configuration
+  ) {
+    super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+    isInPiPMode = isInPictureInPictureMode
+  }
+
+  /**
+    * Align the back button behavior with Android S
+    * where moving root activities to background instead of finishing activities.
+    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+    */
+  override fun invokeDefaultOnBackPressed() {
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+          if (!moveTaskToBack(false)) {
+              // For non-root activities, use the default implementation to finish them.
+              super.invokeDefaultOnBackPressed()
+          }
+          return
+      }
+
+      // Use the default back button implementation on Android S
+      // because it's doing more than [Activity.moveTaskToBack] in fact.
+      super.invokeDefaultOnBackPressed()
+  }
+}
+
---
+++ android/app/src/main/java/com/pomomate/app/PiPModule.kt
@@ -0,0 +1,45 @@
+package com.pomomate.app
+
+import android.os.Build
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class PiPModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+  override fun getName(): String = "PiPModule"
+
+  @ReactMethod
+  fun enterPiPMode(promise: Promise) {
+    val activity = currentActivity as? MainActivity
+    if (activity == null) {
+      promise.reject("NO_ACTIVITY", "Activity not found")
+      return
+    }
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+      promise.reject("NOT_SUPPORTED", "PiP is not supported on this Android version")
+      return
+    }
+
+    try {
+      activity.runOnUiThread {
+        activity.enterPiPMode()
+        promise.resolve(true)
+      }
+    } catch (e: Exception) {
+      promise.reject("PIP_ERROR", "Failed to enter PiP mode: ${e.message}")
+    }
+  }
+
+  @ReactMethod
+  fun isPiPSupported(promise: Promise) {
+    promise.resolve(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+  }
+
+  @ReactMethod
+  fun isInPiPMode(promise: Promise) {
+    promise.resolve(MainActivity.isInPiPMode)
+  }
+}
